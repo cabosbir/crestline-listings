0740e657cbd584a3d142f4de7e460e5f6ad8c6a3
cabosbir <cabosbir@gmail.com>
2025-10-25 16:24:15 -0700

    Add ScrollToTop component to fix navigation scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import Properties from "./pages/Properties";
 import About from "./pages/About";
@@ -13,12 +15,24 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+// ScrollToTop component - scrolls to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/properties" element={<Properties />} />
